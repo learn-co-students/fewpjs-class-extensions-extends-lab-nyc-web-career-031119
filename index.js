18910b1096a28337109e7e2c42386a08ef54a790
John Lee <jslee1992@gmail.com>
2019-05-02 23:16:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+class Polygon {
+  constructor(arr){
+    this.arr = arr;
+  }
+
+  get getCount(){
+    return this.arr.length;
+  }
+
+  get perimeter(){
+    return this.arr.reduce((agg, el) => agg + el);
+  }
+
+}
+
+class Triangle extends Polygon {
+  get isValid(){
+    let largestNum = Math.max(...this.arr);
+    return (this.perimeter - largestNum) > largestNum;
+  }
+}
+
+class Square extends Polygon {
+  get isValid(){
+    return this.perimeter/4 === this.arr[0];
+  }
+  get area(){
+    return this.arr[0] ** 2
+  }
+}
